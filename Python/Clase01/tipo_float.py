15f40cea0b2c3337872595cf9e0ffa585d4e4c61
Juan Schallmoser <schallmoserjuan@gmail.com>
2023-09-25 19:03:07 -0300

    Valor infinito positivo y negativo

--- Python/Clase01/tipo_float.py
+++ Python/Clase01/tipo_float.py
@@ -12,4 +12,12 @@ print(f"El valor de c es: {c:.2f}")
 
 # Notacion exponencial (valores positivos y negativos)
 a = 3e5
-print(f'a: {a}')
+print(f'a: {a:.2f}')
+
+b = 3e-5
+print(f'b: {b:.2f}')
+
+# Cualquier calculo que incluye un float, todo cambia a float
+a = 3.0 + 2
+print(f'a: {a:.2f}')
+print(f'El tipo de a es: {type(a)}')

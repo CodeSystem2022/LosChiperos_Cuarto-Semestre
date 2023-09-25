9b7eadeab8fad64bea26f464db54fee6dc035335
Juan Schallmoser <96781254+SchallmoserJuan@users.noreply.github.com>
2023-09-25 18:45:58 -0300

    Add files via upload

---
+++ Python/Clase01/Sistema_numeracion.py
@@ -0,0 +1,24 @@
+# Profundizando en los sistemas de numeración
+
+# Sistema decimal
+a = 10
+
+print(f"El valor de a es: {a}")
+
+
+# Sistema binario
+b = 0b1010
+
+print(f"El valor de b es: {b}")
+
+
+# Sistema octal
+c = 0o12
+
+print(f"El valor de c es: {c}")
+
+# Sistema hexadecimal
+d = 0xA
+
+print(f"El valor de d es: {d}")
+

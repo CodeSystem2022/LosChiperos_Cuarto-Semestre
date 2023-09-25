4bbeea852c5acc890c5b84f8cd1ed555ef312d2a
Juan Schallmoser <schallmoserjuan@gmail.com>
2023-09-25 19:08:16 -0300

    Valores infinity -infinity

--- Python/Clase01/valores_infinitos.py
+++ Python/Clase01/valores_infinitos.py
@@ -8,3 +8,21 @@ print(f'Es infinito? {math.isinf(infinito_positivo)}')
 infinito_negativo = float("-inf")
 print(f"El valor de infinito_negativo es: {infinito_negativo}")
 print(f'Es infinito? {math.isinf(infinito_negativo)}')
+
+#Modulo Math
+infinito_positivo = math.inf
+print(f"El valor de infinito_positivo es: {infinito_positivo}")
+print(f'Es infinito? {math.isinf(infinito_positivo)}')
+
+infinito_negativo = -math.inf
+print(f"El valor de infinito_negativo es: {infinito_negativo}")
+print(f'Es infinito? {math.isinf(infinito_negativo)}')
+
+#Modulo decimal
+infinito_positivo = Decimal("Infinity")
+print(f"El valor de infinito_positivo es: {infinito_positivo}")
+print(f'Es infinito? {math.isinf(infinito_positivo)}')
+
+infinito_negativo = Decimal("-Infinity")
+print(f"El valor de infinito_negativo es: {infinito_negativo}")
+print(f'Es infinito? {math.isinf(infinito_negativo)}')
